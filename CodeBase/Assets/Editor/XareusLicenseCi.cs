980dca640b1738cdaabdef09628d17f87852cfc1
djedje-cyber <gerrylongfils@gmail.com>
2025-11-12 15:22:17 +0100

    fix(XareusLicenseCi.cs): fix the license entry

--- CodeBase/Assets/Editor/XareusLicenseCi.cs
+++ CodeBase/Assets/Editor/XareusLicenseCi.cs
@@ -8,12 +8,16 @@ public class XareusLicenseSetup
     static XareusLicenseSetup()
     {
         string licenseContent = System.Environment.GetEnvironmentVariable("XAREUS_LICENSE");
+        Debug.Log(licenseContent);
         if (!string.IsNullOrEmpty(licenseContent))
         {
             Xareus.Unity.Licensing.LicenseManager.CheckLicense(licenseContent);
             UnityEngine.Debug.Log("Xareus license written from environment variable.");
         }
-        
+        else
+        {
+            Debug.LogWarning("XAREUS_LICENSE environment variables is not set. Xareus license not configured.");
+        }
     }
 }
 
